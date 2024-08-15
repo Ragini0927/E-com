d3a5710fbea2fdf6ccf9853e67e2eb9daef9ce9f
Ragini0927 <92200080+Ragini0927@users.noreply.github.com>
2024-08-15 20:27:37 +0530

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import Header from './Header'; // Import Header component
+import Footer from './Footer'; // Import Footer component
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ProductList({ user, addToCart, removeFromCart, cart }) {
@@ -19,81 +21,45 @@ function ProductList({ user, addToCart, removeFromCart, cart }) {
   };
 
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">E-commerce</Link>
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-toggle="collapse" 
-          data-target="#navbarNav" 
-          aria-controls="navbarNav" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/cart">Cart</Link>
-            </li>
-          </ul>
+    <div>
+      {/* Header */}
+      <Header user={user} handleLogout={handleLogout} />
 
-          {/* Right-aligned part */}
-          <ul className="navbar-nav ml-auto">
-            {user ? (
-              <>
-                <li className="nav-item">
-                  <span className="navbar-text">Welcome, {user.name}</span>
-                </li>
-                <li className="nav-item">
-                  <button className="btn btn-outline-danger ml-2" onClick={handleLogout}>Logout</button>
-                </li>
-              </>
-            ) : (
-              <li className="nav-item">
-                <Link className="btn btn-outline-primary ml-2" to="/login">Login</Link>
-              </li>
-            )}
-          </ul>
-        </div>
-      </nav>
-
-      <div className="row mt-2">
-        {products.map(product => (
-          <div className="col-md-4" key={product.id}>
-            <div className="card mb-4">
-              <img 
-                src={product.image} 
-                className="card-img-top" 
-                alt={product.name} 
-                style={{ height: '200px', objectFit: 'fit' }} 
-              />
-              <div className="card-body">
-                <h5 className="card-title">{product.title}</h5>
-                <p className="card-text">${product.price}</p>
-                <button 
-                  className="btn btn-primary mr-2" 
-                  onClick={() => addToCart(product)}>
-                  Add
-                </button>
-                <button 
-                  className="btn btn-danger" 
-                  onClick={() => removeFromCart(product)}>
-                  Remove
-                </button>
+      <div className="container">
+        <div className="row mt-2">
+          {products.map(product => (
+            <div className="col-md-4" key={product.id}>
+              <div className="card mb-4">
+                <img
+                  src={product.image}
+                  className="card-img-top"
+                  alt={product.name}
+                  style={{ height: '200px', objectFit: 'fit' }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <p className="card-text">${product.price}</p>
+                  <button
+                    className="btn btn-primary mr-2"
+                    onClick={() => addToCart(product)}
+                  >
+                    Add
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeFromCart(product)}
+                  >
+                    Remove
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
