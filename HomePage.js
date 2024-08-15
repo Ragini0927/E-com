d452dd572365dd9b0d07694a9fffffc6bd04aed9
Ragini0927 <92200080+Ragini0927@users.noreply.github.com>
2024-08-15 15:19:05 +0530

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,37 +1,76 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductCart from './ProductCart';
 
-class HomePage extends Component {
-  state = {
-    products: [],
-  };
+function ProductList({ addToCart, removeFromCart }) {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-  componentDidMount() {
-    // Fetch products from an API
-    axios.get("https://fakestoreapi.com/products").then((response) => {
-      this.setState({ products: response.data });
-    });
-  }
+  useEffect(() => {
+    axios.get('http://localhost:3000/products')
+      .then(response => setProducts(response.data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
 
-  render() {
-    const { user, addToCart } = this.props;
-    const { products } = this.state;
+  return (
+    <div className="container" >
+      <h2>E-commerce</h2>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    
+  <div style={{float: "right "}} >Welcome, User</div>
 
-    return (
-      <div>
-        <h2>Welcome, {user ? user.name : "Guest"}!</h2>
-        <h3>Products</h3>
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>
-              {product.title} - ${product.price}
-              <button onClick={() => addToCart(product)}>Add to Cart</button>
-            </li>
-          ))}
-        </ul>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href={<ProductCart cart={cart}></ProductCart>}>Cart
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+      <div className="row mt-2">
+        {products.map(product => (
+          <div className="col-md-4" key={product.id}>
+            <div className="card mb-4">
+              <img 
+                src={product.image} 
+                className="card-img-top" 
+                alt={product.name} 
+                style={{ height: '200px', objectFit: 'fit' }} 
+              />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">${product.price}</p>
+                <button 
+                  className="btn btn-primary mr-2" 
+                  onClick={() => addToCart(product)}>
+                  Add
+                </button>
+                <button 
+                  className="btn btn-danger" 
+                  onClick={() => removeFromCart(product)}>
+                  Remove
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default HomePage;
+export default ProductList;
