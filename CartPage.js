9cb39e2eae681f39db3b093af34974a15176b092
Ragini0927 <92200080+Ragini0927@users.noreply.github.com>
2024-08-15 15:19:36 +0530

    Update CartPage.js

--- CartPage.js
+++ CartPage.js
@@ -1,26 +1,38 @@
-import React, { Component } from "react";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class CartPage extends Component {
-  render() {
-    const { cart } = this.props;
 
-    return (
-      <div>
-        <h2>Your Cart</h2>
-        <ul>
-          {cart.length > 0 ? (
-            cart.map((item, index) => (
-              <li key={index}>
-                {item.title} - ${item.price}
-              </li>
-            ))
-          ) : (
-            <p>Your cart is empty.</p>
-          )}
+function ProductCart({ cart }) {
+  const totalAmount = cart.reduce((total, product) => total + product.price, 0);
+
+  return (
+    <div className="container">
+      <h2>Your Cart</h2>
+      {cart.length === 0 ? (
+        <p>No items in the cart</p>
+      ) : (
+        <ul className="list-group mb-3">
+          {cart.map(product => (
+            <li className="list-group-item d-flex justify-content-between lh-condensed" key={product.id}>
+              <div>
+              <img 
+                src={product.image} 
+                className="card-img-top" 
+                alt={product.name} 
+                style={{ height: '200px', objectFit: 'fit' }} 
+              />
+                <h6 className="my-0">{product.title}</h6>
+              </div>
+              <span className="text-muted">${product.price}</span>
+            </li>
+          ))}
+         
         </ul>
-      </div>
-    );
-  }
+      )}
+      <h4>Total: ${totalAmount}</h4>
+      <button className="btn btn-success btn-lg btn-block">Proceed to Payment</button>
+    </div>
+  );
 }
 
-export default CartPage;
+export default ProductCart;
