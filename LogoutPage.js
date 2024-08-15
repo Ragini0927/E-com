041bb77b56ed314230d81e5ca8cabcb450137ea5
Ragini0927 <92200080+Ragini0927@users.noreply.github.com>
2024-08-15 19:14:45 +0530

    Update LogoutPage.js

--- LogoutPage.js
+++ LogoutPage.js
@@ -1,17 +1,26 @@
-import React, { Component } from "react";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class LogoutPage extends Component {
-  componentDidMount() {
-    this.props.handleLogout();
-  }
+function Logout() {
+  const navigate = useNavigate();
 
-  render() {
-    return (
-      <div>
-        <h2>You have been logged out</h2>
+  const handleLogout = () => {
+    // Clear user session or token
+    // Redirect to login page
+    navigate('/login');
+  };
+
+  return (
+    <div className="container d-flex flex-column justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <div className="card shadow p-4 text-center" style={{ maxWidth: '400px', width: '100%' }}>
+        <h2 className="mb-4">You have been logged out!</h2>
+        <button className="btn btn-primary btn-lg w-100" onClick={handleLogout}>
+          Go to Login
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default LogoutPage;
+export default Logout;
